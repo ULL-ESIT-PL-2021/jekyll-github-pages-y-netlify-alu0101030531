a63408bbc10fdcc1f00544ce17e935a7ce41a6fe
Christian Pérez Hdez <alu0101030531@ull.edu.es>
2021-03-22 22:00:17 +0000

    uncommented remote

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Banana",
+        "excerpt":"A banana is an edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called “plantains”, distinguishing them from dessert bananas. The fruit is variable in size, color, and firmness, but...","categories": [],
+        "tags": [],
+        "url": "/banana/",
+        "teaser": null
+      }]
