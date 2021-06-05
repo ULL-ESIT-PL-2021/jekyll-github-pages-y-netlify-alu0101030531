724fcb870c9d0900cdf057f7928189cc527629b5
Christian Pérez Hdez <alu0101030531@ull.edu.es>
2021-06-05 20:30:01 +0000

    fixed baseurl bugs

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,42 +2,42 @@ var store = [{
         "title": "February",
         "excerpt":"Fotos de febrero  ","categories": [],
         "tags": [],
-        "url": "/months/february/",
+        "url": "/jekyll-github-pages-y-netlify-alu0101030531/months/february/",
         "teaser": null
       },{
         "title": "January",
         "excerpt":"Fotos de enero  ","categories": [],
         "tags": [],
-        "url": "/months/january/",
+        "url": "/jekyll-github-pages-y-netlify-alu0101030531/months/january/",
         "teaser": null
       },{
         "title": "March",
         "excerpt":"Fotos de marzo  ","categories": [],
         "tags": [],
-        "url": "/months/march/",
+        "url": "/jekyll-github-pages-y-netlify-alu0101030531/months/march/",
         "teaser": null
       },{
         "title": "Amce City",
         "excerpt":"                                               ","categories": [],
         "tags": ["europe","landscape"],
-        "url": "/2021-04-09-Amce-City/",
+        "url": "/jekyll-github-pages-y-netlify-alu0101030531/Amce-City/",
         "teaser": null
       },{
         "title": "Rahbor city",
         "excerpt":"                                               ","categories": [],
         "tags": ["america"],
-        "url": "/2021-04-09-Rahbor/",
+        "url": "/jekyll-github-pages-y-netlify-alu0101030531/Rahbor/",
         "teaser": null
       },{
         "title": "Ruyamur",
         "excerpt":"                                               ","categories": [],
         "tags": ["america"],
-        "url": "/2021-04-09-Ruyamur/",
+        "url": "/jekyll-github-pages-y-netlify-alu0101030531/Ruyamur/",
         "teaser": null
       },{
         "title": "Kaká",
         "excerpt":"                                               ","categories": [],
         "tags": ["landscape"],
-        "url": "/2021-04-09-kaka/",
+        "url": "/jekyll-github-pages-y-netlify-alu0101030531/kaka/",
         "teaser": null
       }]
